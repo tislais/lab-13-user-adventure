066a07d23507f4146b6a466ec188c22faf1ca818
Tis Lais <christinalais@gmail.com>
2021-04-12 15:53:28 -0700

    add story assets and death cause to results page

--- quest/quest.js
+++ quest/quest.js
@@ -12,7 +12,7 @@ const audioSource = document.getElementById('audio-source');
 
 
 if (user.age < 10) {
-    audioSource.src = '../assets/music_03.ogg';
+    audioSource.src = '../assets/music_01.ogg';
 } else if (user.age >= 10 && user.age < 20) {
     audioSource.src = '../assets/music_08.ogg';
 } else if (user.age >= 20 && user.age < 30) {
@@ -31,13 +31,32 @@ const questDiv = document.getElementById('quest-div');
 const params = new URLSearchParams(window.location.search);
 const questId = params.get('id');
 const quest = findById(quests, questId);
-const image = document.createElement('img');
+const image = document.createElement('div');
 const h3 = document.createElement('h3');
 const pIntro = document.createElement('p');
 const button = document.createElement('button');
 const pQuestion = document.createElement('p');
 
-image.src = `../assets/${quest.image}`;
+image.classList.add('quest-image');
+image.style.backgroundImage = `url(../assets/${quest.image})`;
+
+if (quest.id === 'home') {
+    switch (user.expertise) {
+        case 'arctic':
+            image.style.backgroundImage = `url(../assets/${quest.imageArctic})`;
+            break;
+        case 'language':
+            image.style.backgroundImage = `url(../assets/${quest.imageLanguage})`;
+            break;
+        case 'jungle':
+            image.style.backgroundImage = `url(../assets/${quest.imageJungle})`;
+            break;
+        case 'desert':
+            image.style.backgroundImage = `url(../assets/${quest.imageDesert})`;
+            break;
+    }
+}
+
 h3.textContent = quest.title;
 pIntro.textContent = quest.description;
 
@@ -60,7 +79,7 @@ pQuestion.textContent = `How will you spend your time at ${quest.title}?`;
 
 button.textContent = 'Choose';
 form.append(button);
-questDiv.append(h3, image, pIntro, pQuestion, form); 
+questDiv.append(image, h3, pIntro, pQuestion, form); 
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
--- results/results.js
+++ results/results.js
@@ -14,9 +14,20 @@ const image = document.createElement('img');
 image.src = '../assets/dead.png';
 youDiedDiv.textContent = `You died.`;
 ageDiv.textContent = `Age: ${user.age} years`;
-causeDiv.textContent = `Cause: ${user.death}`;
+causeDiv.textContent = `Cause: ${user.deathCause}`;
 button.textContent = 'ok';
-familyDiv.textContent = `Family: Thriving`;
+
+if (user.hp <= 1) {
+    familyDiv.textContent = `Family: Dead`;
+} else if (user.hp > 1 && user.hp <= 5) {
+    familyDiv.textContent = `Family: Dying`;
+} else if (user.hp > 5 && user.hp <= 20) {
+    familyDiv.textContent = `Family: Struggling`;
+} else if (user.hp > 20 && user.hp < 40) {
+    familyDiv.textContent = `Family: Growing`;
+} else if (user.hp >= 40) {
+    familyDiv.textContent = `Family: Thriving`;
+}
 
 
 
