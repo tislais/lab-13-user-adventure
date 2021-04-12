5cb32d7b24c97ab1d442868c83119ee8a3855722
Tis Lais <christinalais@gmail.com>
2021-04-12 12:03:21 -0700

    hide show map elements based on quest completion

--- map/map.js
+++ map/map.js
@@ -10,7 +10,7 @@ renderHeader();
 const audio = document.getElementById('audio');
 const audioSource = document.getElementById('audio-source');
 audioSource.src = '../assets/music_01.ogg';
-audio.load();
+//audio.load();
 
 const isDead = function(user) {
     if (user.dead === true) return true;
@@ -24,5 +24,25 @@ for (let quest of quests) {
     questLink.textContent = quest.title;
     questLink.href = `../quest/?id=${quest.id}`;
     questLink.classList.add(`${quest.id}-link`);
-    mapDiv.append(questLink);
+
+    const completed = user.completed[quest.id];
+    const completedLink = document.createElement('div');
+    completedLink.classList.add(`completed`);
+    completedLink.classList.add(`${quest.id}-completed`);
+    completedLink.textContent = quest.title;
+
+    questLink.classList.remove('hidden');
+    completedLink.classList.add('hidden');
+
+    if (completed) {
+        questLink.classList.add('hidden');
+        completedLink.classList.remove('hidden');
+    }
+
+    if (user.age < 10 && quest.id !== 'home') {
+        questLink.classList.add('hidden');
+    } else if (user.age < 50 && quest.id === 'apocalypse') {
+        questLink.classList.add('hidden');
+    } 
+    mapDiv.append(questLink, completedLink);
 }
